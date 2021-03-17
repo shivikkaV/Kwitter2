bad56680bf3edc4d6a9f46640f91326f0b3b6c1c
shivikkaV <74470668+shivikkaV@users.noreply.github.com>
2021-03-17 08:28:17 -0400

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBgERWfOLr9ca7K6v54YWWdAygu2AnIw8g",
+    authDomain: "kwitterprojects.firebaseapp.com",
+    projectId: "kwitterprojects",
+    storageBucket: "kwitterprojects.appspot.com",
+    messagingSenderId: "35634274818",
+    appId: "1:35634274818:web:5908c3d2eb2ba9db6f7d1b",
+    measurementId: "G-7Q9RY06FB1"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
